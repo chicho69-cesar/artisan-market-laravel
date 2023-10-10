11f5f367e9f6ffa6df65565a8a8445a4489b7ece
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-10-10 07:54:02 -0600

    Create address model and migration

---
+++ app/Models/Address.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model {
+  use HasFactory;
+
+  protected $fillable = [
+    'street',
+    'no_out',
+    'no_in',
+    'zip_code',
+    'city',
+    'state',
+    'country',
+    'phone',
+  ];
+}
